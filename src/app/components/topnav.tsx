68fde178509c58960f91506948e032d9525bfe0d
Tobi Balogun <tobibags19@gmail.com>
2025-10-24 12:07:38 -0500

    CHORE: Change search bar background opacity, and format with prettier

--- src/app/components/topnav.tsx
+++ src/app/components/topnav.tsx
@@ -13,24 +13,24 @@ export default function TopNav() {
           height={50}
         />
 
-        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start p-2">
+        <div className="flex flex-1 items-center justify-center p-2 sm:items-stretch sm:justify-start">
           <div className="flex space-x-4">
             <a
               href="#"
               aria-current="page"
-              className="rounded-md bg-gray-800/50 px-3 py-2 text-sm font-medium"
+              className="rounded-md bg-gray-800/30 px-3 py-2 text-sm font-medium"
             >
               Games
             </a>
             <a
               href="#"
-              className="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700/50 hover:text-white"
+              className="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700/30 hover:text-white"
             >
               Standings
             </a>
             <a
               href="#"
-              className="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700/50 hover:text-white"
+              className="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700/30 hover:text-white"
             >
               Stats
             </a>
@@ -41,7 +41,7 @@ export default function TopNav() {
           <Input
             type="search"
             placeholder="Search..."
-            className="w-64 rounded-md border-gray-700/30 bg-gray-800/10 px-3 py-2 text-sm text-white placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500"
+            className="w-64 rounded-md border-gray-700/30 bg-gray-800/10 px-3 py-2 text-sm text-black placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500"
           />
           <Button>Search</Button>
         </div>
