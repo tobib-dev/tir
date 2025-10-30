c9e5bb36b7f2a5f077c576ced9c7272b635df318
Tobi Balogun <tobibags19@gmail.com>
2025-10-30 11:52:13 -0500

    CHORE: Create query get live games, and isolate upcoming games from live games

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,11 +1,15 @@
 import { db } from "./db";
 import { type NewGame, games } from "./db/schema";
-import { asc } from "drizzle-orm";
+import { sql, asc, gt, lte } from "drizzle-orm";
 
 export async function createGame(game: NewGame) {
   await db.insert(games).values(game);
 }
 
-export async function getGames() {
-  return await db.select().from(games).orderBy(asc(games.startTime));
+export async function getUpcomingGames() {
+  return await db.select().from(games).where(gt(games.startTime, new Date())).orderBy(asc(games.startTime));
+}
+
+export async function getLiveGames() {
+  return await db.select().from(games).where(lte(games.startTime, sql`CURRENT_TIMESTAMP + INTERVAL '2 hours'`)).orderBy(asc(games.startTime));
 }
